d45d449fc004315f51dd53b5938da9b9db6e287a
asmii123 <asmimishra123@gmail.com>
2022-01-31 11:46:34 +0545

    changes

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,14 @@
 import React from "react"
-import memesData from "../memesData.js"
+
 
 export default function Meme() {
     const [memeImage, setMemeImage] = React.useState("")
     
     async function  getMemeImage() {
-        // const memesArray = memesData.data.memes
-        // const randomNumber = Math.floor(Math.random() * memesArray.length)
-        // setMemeImage(memesArray[randomNumber].url)
+     
         const responsePromise = await fetch('https://meme-api.herokuapp.com/gimme');
         const responseJSON = await responsePromise.json();
-       // return responseJSON.result;
+   
        setMemeImage(responseJSON.url)
 
     }
@@ -18,24 +16,16 @@ export default function Meme() {
     return (
         <main>
             <div className="form">
-                <input 
-                    type="text"
-                    placeholder="Top text"
-                    className="form--input"
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom text"
-                    className="form--input"
-                />
+    
                 <button 
                     className="form--button"
                     onClick={getMemeImage}
                 >
                     Get a new meme image 🖼
                 </button>
-            </div>
+                </div>
             <img src={memeImage} className="meme--image" />
+            
         </main>
     )
 }
